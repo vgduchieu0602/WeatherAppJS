343d9e19bf94a455aaa006a6ba82e58811023db7
Vương Đức Hiếu <vg.dhieu0602@gmail.com>
2026-03-19 06:59:22 +0700

    finish

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const cityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-btn");
 
+const weatherInfoSection = document.querySelector(".weather-info");
+const notFoundSection = document.querySelector(".not-found");
+const searchCitySection = document.querySelector(".search-city");
+
+const countryTxt = document.querySelector(".country-txt");
+const tempTxt = document.querySelector(".temp-txt");
+const conditionTxt = document.querySelector(".condition-txt");
+const humidityValueTxt = document.querySelector(".humidity-value-txt");
+const windValueTxt = document.querySelector(".wind-value-txt");
+const weatherSummaryImg = document.querySelector(".weather-summary-img");
+const currentDateTxt = document.querySelector(".current-date-txt");
+
+const forecastItemsContainer = document.querySelector(
+  ".forecast-items-container",
+);
+
 const apiKey = "ad55e6df9cfc6e5f9738143a7d1218e7";
 
 searchBtn.addEventListener("click", () => {
@@ -19,8 +35,113 @@ cityInput.addEventListener("keydown", (e) => {
   }
 });
 
-function getFetchData() {}
+async function getFetchData(endPoint, city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
 
-function updateWeatherInfo(city) {
-  const weatherData = getFetchData();
+  const response = await fetch(apiUrl);
+
+  return response.json();
+}
+
+function getWeatherIcon(id) {
+  if (id <= 232) return "thunderstorm.svg";
+  if (id <= 321) return "drizzle.svg";
+  if (id <= 531) return "rain.svg";
+  if (id <= 622) return "snow.svg";
+  if (id <= 781) return "atmosphere.svg";
+  if (id <= 800) return "clear.svg";
+  else return "clouds.svg";
+}
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  const options = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+  };
+
+  return currentDate.toLocaleDateString("vi-VN", options);
+}
+
+async function updateWeatherInfo(city) {
+  const weatherData = await getFetchData("weather", city);
+  if (weatherData.cod != 200) {
+    showDisplaySection(notFoundSection);
+    return;
+  }
+  console.log(weatherData);
+
+  const {
+    name: country,
+    main: { temp, humidity },
+    weather: [{ id, main }],
+    wind: { speed },
+  } = weatherData;
+
+  countryTxt.textContent = country;
+  tempTxt.textContent = Math.round(temp) + " °C";
+  conditionTxt.textContent = main;
+  humidityValueTxt.textContent = humidity + "%";
+  windValueTxt.textContent = speed + " M/s";
+
+  currentDateTxt.textContent = getCurrentDate();
+
+  weatherSummaryImg.src = `/assets/weather/${getWeatherIcon(id)}`;
+
+  await updateForecastsInfo(city);
+  showDisplaySection(weatherInfoSection);
+}
+
+async function updateForecastsInfo(city) {
+  const forecastsData = await getFetchData("forecast", city);
+  const timeTaken = "12:00:00";
+  const todayDate = new Date().toISOString().split("T")[0];
+
+  forecastItemsContainer.innerHTML = "";
+  forecastsData.list.forEach((forecastWeather) => {
+    if (
+      forecastWeather.dt_txt.includes(timeTaken) &&
+      !forecastWeather.dt_txt.includes(todayDate)
+    ) {
+      updateForecastItems(forecastWeather);
+    }
+  });
+}
+
+function updateForecastItems(weatherData) {
+  const {
+    dt_txt: date,
+    weather: [{ id }],
+    main: { temp },
+  } = weatherData;
+
+  const dateTaken = new Date(date);
+  const dateOptions = {
+    day: "2-digit",
+    month: "short",
+  };
+  const dateResult = dateTaken.toLocaleDateString("vi-VN", dateOptions);
+
+  const foreCastItem = `
+    <div class="forecast-item">
+      <h5 class="forecast-item-date regular-txt">${dateResult}</h5>
+      <img
+        src="./assets/weather/${getWeatherIcon(id)}"
+        alt=""
+        class="forecast-item-img"
+      />
+      <h5 class="forecast-item-temp">${Math.round(temp)} °C</h5>
+    </div>
+  `;
+
+  forecastItemsContainer.insertAdjacentHTML("beforeend", foreCastItem);
+}
+
+function showDisplaySection(section) {
+  [weatherInfoSection, notFoundSection, searchCitySection].forEach(
+    (section) => (section.style.display = "none"),
+  );
+
+  section.style.display = "flex";
 }
